6d9ca8d9d02c7adcc6c25ba389b172573993e49d
luana <luana.silva983@etec.sp.gov.br>
2023-09-11 10:15:45 -0300

    arquivo enviado

---
+++ android/app/src/main/kotlin/com/example/etanol_gasoliana/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.etanol_gasoliana
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:etanol_gasoliana/home.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false                                                                       ,
+    home: Home(),
+  ));
+}
